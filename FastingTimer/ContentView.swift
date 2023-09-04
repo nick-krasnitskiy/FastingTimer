573ca754a8f0c0644fdaf7ad6141da51dfa8d5e5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-04 14:35:43 +0300

    Initial Commit

---
+++ FastingTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Nick Krasnitskiy on 04.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Nick Krasnitskiy on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
