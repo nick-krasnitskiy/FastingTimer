739efca501153d861c460b1d3e09a896ec773318
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-06 14:20:47 +0300

    Add fasting plan states.

--- FastingTimer/ContentView.swift
+++ FastingTimer/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                 
                 // MARK: Fasting Plan
                 
-                Text("16:8")
+                Text(fastingManager.fastingPlan.rawValue)
                     .fontWeight(.semibold)
                     .padding(.horizontal, 24)
                     .padding(.vertical, 8)
--- FastingTimer/FastingManager.swift
+++ FastingTimer/FastingManager.swift
@@ -13,8 +13,26 @@ enum FastingState {
     case feeding
 }
 
+enum FastingPlan: String {
+    case beginner = "12:12"
+    case intermediate = "16:8"
+    case advanced = "20:4"
+    
+    var fastingPeriod: Double {
+        switch self {
+        case .beginner:
+            return 12
+        case .intermediate:
+            return 16
+        case .advanced:
+            return 20
+        }
+    }
+}
+
 class FastingManager: ObservableObject {
     @Published private(set) var fastingState: FastingState = .notStarted
+    @Published private(set) var fastingPlan: FastingPlan = .intermediate
     
     func toggleFastingState() {
         fastingState = fastingState == .fasting ? .feeding : .fasting
